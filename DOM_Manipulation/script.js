28949245384b0c76f5426950c56a847c5af5efa5
jstevens8185 <j.stevens8185@gmail.com>
2022-06-10 10:45:31 -0500

    Add div and style using js

--- DOM_Manipulation/script.js
+++ DOM_Manipulation/script.js
@@ -9,3 +9,24 @@ const h3 = document.createElement('h3');
 h3.textContent =  "I'm a blue h3!";
 h3.style.color = 'blue';
 
+container.append(h3);
+
+const divInContainer = document.createElement('div');
+divInContainer.style.borderColor = 'black';
+divInContainer.style.borderStyle = 'solid';
+divInContainer.style.borderWidth = '2px';
+divInContainer.style.backgroundColor = 'pink';
+
+const h1InDiv = document.createElement('h1');
+h1InDiv.textContent = "I'm in a div";
+
+
+const paraInDiv = document.createElement('p');
+paraInDiv.textContent = "ME TOO!";
+
+divInContainer.append(h1InDiv);
+divInContainer.append(paraInDiv);
+
+container.append(divInContainer);
+
+
