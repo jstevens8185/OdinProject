629896640fde0f3f83a95faebb6733104cad2a9d
jstevens8185 <j.stevens8185@gmail.com>
2022-06-10 10:32:41 -0500

    Add paragraph with red text using JS

---
+++ DOM_Manipulation/script.js
@@ -0,0 +1,6 @@
+const paragraph = document.createElement('p');
+paragraph.textContent = "Hey I'm red!";
+paragraph.style.color = 'red';
+
+const container = document.querySelector('#container');
+body.append(paragraph);
